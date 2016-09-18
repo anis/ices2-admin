852d1b3cf9704868d934d19e03388c9965da8d6e
Anis Safine <anis@safine.me>
2016-09-18 20:28:40 +0200

    Add an index page that can be accessed only if configuration is ok

--- app/config/routing.php
+++ app/config/routing.php
@@ -2,6 +2,20 @@
 $config = require __DIR__ . '/config.php';
 
 $app
+    ->get('/', function () {
+        return '';
+    })
+    ->before(function () use($app, $config) {
+        if (!$app['ezstream']->isInstalled()
+            || !$app['ices']->isInstalled()
+            || !isset($config['ices2_folder'])
+            || !is_dir($config['ices2_folder'])
+            || !is_writeable($config['ices2_folder'])) {
+            return $app->redirect('config');
+        }
+    });
+
+$app
     ->get('/config', function () use($app, $config) {
         return $app['twig']->render('config.twig', array(
             'ezstream' => $app['ezstream']->isInstalled(),
@@ -10,4 +24,5 @@ $app
             'folder_exists' => isset($config['ices2_folder']) ? is_dir($config['ices2_folder']) : false,
             'folder_writeable' => isset($config['ices2_folder']) ? is_writeable($config['ices2_folder']) : false,
         ));
-    });
+    })
+    ->bind('config');
