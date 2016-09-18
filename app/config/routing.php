066eb9a7098566f74882ecc4b169ae7d9a8e8c13
Anis Safine <anis@safine.me>
2016-09-18 19:33:21 +0200

    Fix wrong variable name

--- app/config/routing.php
+++ app/config/routing.php
@@ -4,7 +4,7 @@ $config = require __DIR__ . '/config.php';
 $app
     ->get('/config', function () use($app, $config) {
         return $app['twig']->render('config.twig', array(
-            'installed' => $app['ices']->isInstalled(),
+            'ices2' => $app['ices']->isInstalled(),
             'folder' => isset($config['ices2_folder']) ? $config['ices2_folder'] : 'missing',
             'folder_exists' => isset($config['ices2_folder']) ? is_dir($config['ices2_folder']) : false,
             'folder_writeable' => isset($config['ices2_folder']) ? is_writeable($config['ices2_folder']) : false,
