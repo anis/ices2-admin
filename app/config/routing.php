44f63a9fb80d457975d0b3741e6b967b0ce0414b
Anis Safine <anis@safine.me>
2016-09-18 19:06:05 +0200

    Give the configuration panel a bit of style

--- app/config/routing.php
+++ app/config/routing.php
@@ -4,9 +4,9 @@ $config = require __DIR__ . '/config.php';
 $app
     ->get('/config', function () use($app, $config) {
         return $app['twig']->render('config.twig', array(
-            'installed' => $app['ices']->isInstalled() ? 'true' : 'false',
+            'installed' => $app['ices']->isInstalled(),
             'folder' => isset($config['ices2_folder']) ? $config['ices2_folder'] : 'missing',
-            'folder_exists' => isset($config['ices2_folder']) ? (is_dir($config['ices2_folder']) ? 'true' : 'false') : 'false',
-            'folder_writeable' => isset($config['ices2_folder']) ? (is_writeable($config['ices2_folder']) ? 'true' : 'false') : 'false',
+            'folder_exists' => isset($config['ices2_folder']) ? is_dir($config['ices2_folder']) : false,
+            'folder_writeable' => isset($config['ices2_folder']) ? is_writeable($config['ices2_folder']) : false,
         ));
     });
