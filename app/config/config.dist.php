5da146c1c4945c4bcf2fdae994fbb1f9e82886a3
Anis Safine <anis@safine.me>
2016-09-18 18:12:46 +0200

    Show the configured path for ices2 configuration

---
+++ app/config/config.dist.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'ices2_folder' => '/etc/ices2',
+);
--- app/config/routing.php
+++ app/config/routing.php
@@ -1,7 +1,10 @@
 <?php
+$config = require __DIR__ . '/config.php';
+
 $app
-    ->get('/config', function () use($app) {
+    ->get('/config', function () use($app, $config) {
         return $app['twig']->render('config.twig', array(
-            'installed' => $app['ices']->isInstalled() ? 'true' : 'false'
+            'installed' => $app['ices']->isInstalled() ? 'true' : 'false',
+            'folder' => isset($config['ices2_folder']) ? $config['ices2_folder'] : 'missing',
         ));
     });
